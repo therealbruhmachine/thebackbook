3ad4b4752f7e186dadafbcf9eadddcaf7694b3d9
Dylan Corona <106871151+therealbruhmachine@users.noreply.github.com>
2022-06-04 10:18:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
+<html>
 
+  <head>
+
+    <title>Thebackbook | Welcome to Thebackbook!</title>
+
+    <link rel="stylesheet" href="style.css">
+
+     <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+  </head>
+
+  <body>
+
+  <div class="page-container">
+
+    <div id="header">
+
+    <img src="images/logo-left.jpg" id="logo-left">
+
+    </div>    
+
+    <div class="thebookbox">
+
+    <header class="content-title">Welcome to Thebackbook!</header>
+
+    <div class="content">
+
+      <p>Coming soon!</p>
+
+      <a href="whatitis.php">What is it?</a>
+
+    </div>
+
+    </div>
+
+  </div>
+
+  </body>
+
+</html>
+
+<!-- site looks good thank u -->
