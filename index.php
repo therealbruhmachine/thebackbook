2929da9907d43c83c4c0447c7c25e82a8d557752
Dylan Corona <106871151+therealbruhmachine@users.noreply.github.com>
2022-06-04 11:00:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,19 +16,67 @@
 
     <div id="header">
 
-    <img src="images/logo-left.jpg" id="logo-left">
+    <img src="images/logo-left.jpg" id="logo-left" alt="Mark">
+
+    <div class="header-content">
+
+    <img src="images/thebackbook.png" alt="[Thebackbook]" height="49"><br>
+
+    <a href="login.php">login</a>
+
+    <a href="register.php">register</a>
+
+    <a href="about.php">about</a> 
+
+    </div>
 
     </div>    
 
     <div class="thebookbox">
 
+    <div class="navbar">
+
+    <form action="login.php" method="post">
+
+    <span>Email:</span>
+
+    <input type="email" class="inputtext" name="email" required value="" size="30">
+
+    <span>Password:</span>
+
+    <input type="password" class="inputtext" name="password" required value="" size="30">
+
+    <a href="register.php" class="button-blue">Register</a>
+
+    <button type="submit" class="button-blue">Login</button>  
+
+    </form>
+
+    </div>
+
+    <div class="content-container">
+
     <header class="content-title">Welcome to Thebackbook!</header>
 
     <div class="content">
 
-      <p>Coming soon!</p>
+    <p class="content-welcome">Coming soon!</p>
 
-      <a href="whatitis.php">What is it?</a>
+    <p>Thebackbook is a 2004 Facebook revival, which is in HTML5.<br><br>Currently: the site isn't usable, check back later!</p>
+
+    <br><br><br><br><br>
+
+    <p>To get started, click below to register. If you have already registered, you can log in. (THIS DOESN'T WORK YET. CHECK BACK LATER!)</p>
+
+      <div class="center">
+
+      <a href="register.php" class="button-blue">Register</a>
+
+      <a href="login.php" class="button-blue">Login</a>
+
+      </div>
+
+    </div>
 
     </div>
 
@@ -36,8 +84,19 @@
 
   </div>
 
+<!-- dctv i will make footer myself don't worry -->
+
   </body>
 
 </html>
 
-<!-- site looks good thank u -->
+<!-- k -->
+
+
+
+
+
+
+
+
+
