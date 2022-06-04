e0c79f89dcd3f2411f799656d1905433d5cbf0ce
Dylan Corona <106871151+therealbruhmachine@users.noreply.github.com>
2022-06-04 11:27:58 -0500

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,19 @@
+    <div class="navbar">
+
+    <form action="login.php" method="post">
+
+    <span>Email:</span>
+
+    <input type="email" class="inputtext" name="email" required value="" size="30">
+
+    <span>Password:</span>
+
+    <input type="password" class="inputtext" name="password" required value="" size="30">
+
+    <a href="register.php" class="button-blue">Register</a>
+
+    <button type="submit" class="button-blue">Login</button>  
+
+    </form>
+
+    </div>
